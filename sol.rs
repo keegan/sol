1f23bc85e24ab8cd6fdd90ac7adeba145c7bee59
keegan <swfanboy01@gmail.com>
2024-03-21 21:43:55 -0400

    Selected Cursor Style

--- sol.rs
+++ sol.rs
@@ -1,16 +1,21 @@
 extern crate termion;
 
-//use rand::Rng;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+use std::collections::VecDeque;
 
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use termion::{clear, cursor, style, color};
+use termion::input::Keys;
 
 use std::fmt;
-use std::io::{self, Read, Write};
+use std::io::{self, Read, Write, StdinLock};
 
 const ESC: char = '\x1b';
+const BEL: char = '\x07';
 
 #[derive(Debug)]
 enum Suite {
@@ -109,6 +114,7 @@ fn main() {
         Card { hidden: true, value: 12, suite: Suite::Spade, },
         Card { hidden: true, value: 13, suite: Suite::Spade, },
     ];
+    deck.shuffle(&mut thread_rng());
 
     let stdout = io::stdout();
     let mut stdout = stdout.lock();
@@ -122,10 +128,6 @@ fn main() {
     let termheight = termsize.map(|(_,h)| h - 2).unwrap();
 
 
-    for card in deck.iter() {
-        print!("{card}");
-    }
-
     let mut board: [Vec<usize>; 7] = [
         Vec::new(),
         Vec::new(),
@@ -138,7 +140,7 @@ fn main() {
 
     let mut counter = 0;
 
-    for i in (0..7){
+    for i in 0..7{
         let mut col = &mut board[i];
         for j in (0..=i){
             col.push(counter);
@@ -149,39 +151,138 @@ fn main() {
         }
     }
 
+    let mut drawpile: VecDeque<usize> = (counter..52).collect();
+    deck[*drawpile.get(0).unwrap()].hidden = false;
 
     let mut game = Game {
         width: termwidth,
         stdout: Box::new(stdout),
-        stdin: Box::new(stdin),
+        stdin: Box::new(stdin.keys()),
 
         deck: deck,
         board: board,
+
+        draw: drawpile,
+        foundation: [Vec::new(), Vec::new(), Vec::new(), Vec::new()],
+
+        cursor: 0,
+        selected_card: None,
     };
 
-    game.show();
+    game.run();
 }
 
 /// The game state.
-struct Game{
+struct Game<'a>{
     width: u16,
 
     stdout: Box<dyn Write>,
-    stdin: Box<dyn Read>,
+    stdin: Box<Keys<StdinLock<'a>>>,
 
     deck: [Card; 52],
     board: [Vec<usize>; 7],
+    draw: VecDeque<usize>,
+    foundation: [Vec<usize>; 4],
+
+    cursor: u8,
+    selected_card: Option(usize),
 }
 
-impl Game {
+impl Game<'_> {
     fn show(&mut self){
-        write!(self.stdout, "{}{}{}", clear::All, style::Reset, cursor::Goto(1, 1));
+        write!(self.stdout, "{}{}{}{}{}", clear::All, style::Reset, cursor::Goto(1, 1), color::Fg(color::Reset), color::Bg(color::Reset));
+        match self.draw.get(0) {
+            None => write!(self.stdout, "{}{}|#x#x#x#|{}", color::Bg(color::Yellow), color::Fg(color::Black), style::Reset),
+            Some(x) => write!(self.stdout, "{}", self.deck[*x]),
+        };
+
+        for (i, col) in self.foundation.iter().enumerate() {
+            write!(self.stdout, "{}", cursor::Goto(28 + (i as u16 * 9), 1));
+            match col.last() {
+                None => write!(self.stdout, "{}{}|#x#x#x#|{}", color::Bg(color::Yellow), color::Fg(color::LightYellow), style::Reset),
+                Some(x) => write!(self.stdout, "{}", self.deck[*x]),
+            };
+        }
+
+        //write!(self.stdout, "{1}{0}", self.deck[*card as usize]);
         for (i, col) in self.board.iter().enumerate() {
             for (j, card) in col.iter().enumerate() {
-                write!(self.stdout, "{1}{0}", self.deck[*card as usize], cursor::Goto(1 + (i as u16 * 9), 2 + j as u16));
+                write!(self.stdout, "{1}{0}", self.deck[*card as usize], cursor::Goto(1 + (i as u16 * 9), 3+ j as u16));
             }
         }
-        //write!(self.stdout, "{}.{:?}", cursor::Goto(1, 60), self.board);
+        match self.cursor {
+            card_col @ 0..=6 => write!(self.stdout, "{}", cursor::Goto(1 + (card_col as u16 * 9), 19)),
+            foundation_col @ 7..=10 => write!(self.stdout, "{}", cursor::Goto(28 + ((foundation_col as u16 - 7)* 9), 2)),
+            _ => write!(self.stdout, "{}", cursor::Goto(1, 2)),
+        };
+        write!(self.stdout, "{}{}|=======|", color::Fg(color::Blue), color::Bg(color::Black));
+        write!(self.stdout, "{}{}{}{}", cursor::Goto(15, 1), color::Bg(color::Black), color::Fg(color::White), self.cursor);
+
+        if self.selected_cursor <= 10 {
+            write!(self.stdout, "{}{}{}{}", cursor::Goto(19, 1), color::Bg(color::Black), color::Fg(color::White), self.selected_cursor);
+            match self.selected_cursor {
+                card_col @ 0..=6 => write!(self.stdout, "{}", cursor::Goto(1 + (card_col as u16 * 9), 19)),
+                foundation_col @ 7..=10 => write!(self.stdout, "{}", cursor::Goto(28 + ((foundation_col as u16 - 7)* 9), 2)),
+                _ => Ok(()),
+            };
+            write!(self.stdout, "{}{}", color::Fg(color::Blue), color::Bg(color::White));
+            if self.cursor == self.selected_cursor {
+                write!(self.stdout, "|#=#=#=#|");
+            } else {
+                write!(self.stdout, "|=======|");
+            }
+        }
+
+        write!(self.stdout, "{}{}{}{}", style::Reset, cursor::Goto(1, 1), color::Fg(color::Reset), color::Bg(color::Reset));
+        self.stdout.flush().unwrap();
+    }
+    fn try_move(&mut self){
+        if self.cursor == self.selected_cursor {
+            write!(self.stdout, "{BEL}");
+            write!(self.stdout, "{BEL}");
+            return
+        }
+        let card_to_move = match self.selected_cursor {
+            c_col @ 0..=7 => self.board[c_col].get(),
+        };
+        write!(self.stdout, "{BEL}");
+    }
+    fn run(&mut self){
+        write!(self.stdout, "{}", cursor::Hide);
+        loop {
+            self.show();
+            let b = self.stdin.next().unwrap().unwrap();
+            use termion::event::Key::*;
+            match b {
+                Char('q') | Char('c') => {
+                    write!(self.stdout, "{}{}", style::Reset, cursor::Show);
+                    return
+                },
+                Char('h') | Char('a') | Left  => self.cursor = (self.cursor as i16 - 1).rem_euclid(11) as u8,
+                Char('j') | Char('s') | Down  => self.cursor = match self.cursor {
+                    0..=2 => 7,
+                    card_col @ 3..=6 => card_col,
+                    foundation_col @ 7..=10 => foundation_col - 4,
+                    _ => 0,
+                },
+                Char('k') | Char('w') | Up    => self.cursor = match self.cursor {
+                    0..=2 => 7,
+                    card_col @ 3..=6 => card_col + 4,
+                    foundation_col @ 7..=10 => foundation_col,
+                    _ => 0,
+                },
+                Char('l') | Char('d') | Right => self.cursor = (self.cursor + 1).rem_euclid(11),
+                Char(' ') => {
+                    if self.selected_cursor > 10 {
+                        self.selected_cursor = self.cursor;
+                    } else {
+                        self.try_move();
+                        self.selected_cursor = 11;
+                    }
+                },
+                _ => (),
+            };
+        }
     }
 }
 
